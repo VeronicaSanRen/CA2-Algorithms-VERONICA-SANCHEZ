06b487ed057fee5f86d63daba74d66e335ce2b8a
VeronicaSanRen <2024358@student.cct.ie>
2025-04-30 13:25:02 +0100

    Department

--- src/ca2/algorithms/veronica/sanchez/Department.java
+++ src/ca2/algorithms/veronica/sanchez/Department.java
@@ -9,14 +9,24 @@ package ca2.algorithms.veronica.sanchez;
  * @author dell
  */
 
-//Intensive care
-//Emergency Department
-//Orthopaedics
-//Nursing
-//Cardiology
-//Radiology
+    enum Department {
+    INTENSIVE_CARE("Intensive Care"),
+    EMERGENCY_DEPARTMENT("Emergency Department"),
+    ORTHOPAEDICS("Orthopaedics"),
+    NURSING("Nursing"),
+    CARDIOLOGY("Cardiology"),
+    RADIOLOGY("Radiology");
 
-public class Department {
-    private String name;
-    
+    private final String label;
+    Department(String label) { this.label = label; }
+    @Override public String toString() { return label; }
+
+    public static Department fromString(String text) {
+        for (Department d : Department.values()) {
+            if (d.label.equalsIgnoreCase(text) || d.name().equalsIgnoreCase(text.replace(" ", "_"))) {
+                return d;
+            }
+        }
+        throw new IllegalArgumentException("Invalid Department: " + text);
+    }
 }
